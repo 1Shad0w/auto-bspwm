0f601a9113e660ee60c9083a92010fb8be5fcb0b
Jaibancon <11272098@iesmarxadella.org>
2025-03-29 14:15:42 +0100

    Added Wi-Fi to polybar

--- scripts/ethernet_status.sh
+++ scripts/ethernet_status.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
+# Comprobar la dirección IP de eth0
+IP_ETH0=$(ip addr show eth0 | grep "inet " | awk '{print $2}' | cut -d/ -f1)
+
+# Si no hay dirección IP en eth0, intentar con wlan0
+if [ -z "$IP_ETH0" ]; then
+    IP_WLAN0=$(ip addr show wlan0 | grep "inet " | awk '{print $2}' | cut -d/ -f1)
+    if [ -n "$IP_WLAN0" ]; then
+        echo "%{F#ffffff}  %{F#ffffff}$IP_WLAN0%{u-}"
+    else
+        echo "%{F#ff0000}  %{F#ff0000}No IP found on wlan0%{u-}"
+    fi
+else
+    echo "%{F#ffffff}  %{F#ffffff}$IP_ETH0%{u-}"
+fi
